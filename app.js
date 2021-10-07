f73dd771e87abc2561b74787f086525538d24bc7
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-10-07 23:22:04 +0700

    connect to db

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+require('dotenv').config();
+
 // packages
 const express = require('express');
 
 // express
 const app = express();
 
+const dev = process.env.NODE_ENV !== 'production';
+const MONGO_URL = dev ? process.env.MONGO_URL_TEST : process.env.MONGO_URL;
+
+// database
+const connectDB = require('./db/connect');
+
 const port = process.env.PORT || 5000;
+const ROOT_URL = `http://localhost:${port}`;
 
 const start = async () => {
   try {
-    app.listen(port, console.log(`Server is listening on port ${port}..`));
+    await connectDB(MONGO_URL)
+    app.listen(port, console.log(`Server is listening on port ${ROOT_URL}..`));
   } catch (error) {
     console.log(error);
   }
--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 const connectDB = (url) => {
-  return mongoose.connect(url);
+  return mongoose.connect(url, options);
 };
 
 module.exports = connectDB;
