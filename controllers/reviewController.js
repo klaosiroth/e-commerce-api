d1fa5481a4939f3940a66c6258ab11858ce21d99
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-10-09 01:25:48 +0700

    delete review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -67,7 +67,18 @@ const updateReview = async (req, res) => {
 // Delete Review => DELETE /api/v1/reviews/:id
 
 const deleteReview = async (req, res) => {
-  res.send('delete review');
+  const { id: reviewId } = req.params;
+
+  const review = await Review.findOne({ _id: reviewId });
+
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id: ${reviewId}`);
+  }
+
+  checkPermissions(req.user, review.user);
+
+  await review.remove();
+  res.status(StatusCodes.OK).json({ msg: 'success! review removed' });
 };
 
 module.exports = {
