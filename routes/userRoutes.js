dab4e6531e098bb844bb3ea847979568be3cb48a
natthaphon wichianphong <klaosiroth@gmail.com>
2021-10-08 11:20:00 +0700

    authorize permissions complete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,9 @@ const {
   authorizePermissions,
 } = require('../middleware/authentication');
 
-router.route('/').get(authenticateUser, authorizePermissions, getAllUsers);
+router
+  .route('/')
+  .get(authenticateUser, authorizePermissions('admin'), getAllUsers);
 
 router.route('/showMe').get(showCurrentUser);
 router.route('/updateUser').patch(updateUser);
