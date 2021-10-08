07a05b99aed68ccabb45f6b628fec4aee143adad
natthaphon wichianphong <klaosiroth@gmail.com>
2021-10-08 11:55:17 +0700

    update user password

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,7 +35,23 @@ const updateUser = async (req, res) => {
 };
 
 const updateUserPassword = async (req, res) => {
-  res.send('update user password');
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || !newPassword) {
+    throw new CustomError.BadRequestError('Please provide both values');
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+
+  user.password = newPassword;
+
+  await user.save();
+  res.status(StatusCodes.OK).json({ message: 'Success! Password Updated' });
 };
 
 module.exports = {
