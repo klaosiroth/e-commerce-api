b5ed0da98a417d096b89441e0cd8183cd52d86ae
natthaphon wichianphong <klaosiroth@gmail.com>
2021-10-08 18:23:13 +0700

    remaining product controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,28 +15,54 @@ const createProduct = async (req, res) => {
 // Get All Products => GET /api/v1/products
 
 const getAllProducts = async (req, res) => {
-  res.send('get all products');
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ count: products.length, products });
 };
 
 // Public Route
 // Get Single Product => GET /api/v1/products/:id
 
 const getSingleProduct = async (req, res) => {
-  res.send('get single product');
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ product });
 };
 
 // Protected Route / Admin Only
 // Update Product => PATCH /api/v1/products/:id
 
 const updateProduct = async (req, res) => {
-  res.send('update product');
+  const { id: productId } = req.params;
+  const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id : ${productId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ product });
 };
 
 // Protected Route / Admin Only
 // Delete Product => DELETE /api/v1/products/:id
 
 const deleteProduct = async (req, res) => {
-  res.send('delete product');
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id : ${productId}`);
+  }
+
+  product.remove();
+  res.status(StatusCodes.OK).json({ msg: 'Success! Product Removed' });
 };
 
 // Protected Route / Admin Only
